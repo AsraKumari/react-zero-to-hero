302e35bf6eea2ccb85403722b44e973adef94db9
Asra Kumari <asrakumari24@gmail.com>
2025-05-21 18:49:24 +0530

    Updated project files

--- App.js
+++ App.js
@@ -1,4 +1,29 @@
- //-------------------Using Pure React To Print Heading-------------------//
+/**
+ * 
+ * HOT MODULE RELOADING  (HMR)
+ * File Watcher Algorithms - written in C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning Our Code
+ * Dev and production build
+ * Super fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compressess files
+ * Compatible with older version of browser
+ * HTTPS on dev
+ * Port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * 
+*/
+
+import React from "react"; //core package
+import ReactDOM from "react-dom/client" // package, which is used for manipulating DOM
+
+
+
+//-------------------Using Pure React To Print Heading-------------------//
 
 //create h1 tag and writing text inside heading
 const heading = React.createElement(
@@ -6,9 +31,10 @@ const heading = React.createElement(
         {
 
         id: "title", //what we are passing over here will goes into my heading above
+         key:"h1"   //"key" helps identifying each elements to have a uniquely in a list, it make update faster and avoids UI bugs.
     }, 
 
-    "Heading1" //inside h1
+    "Heading1" //inside h1  //simply it means <h1 id:"title"> Heading1</h1>
 );
 
 //create h2 tag
@@ -17,6 +43,7 @@ const heading2 = React.createElement(
         {
 
         id: "title", //what we are passing over here will goes into my heading above
+        key:"h2",    //"key" helps identifying each elements to have a uniquely in a list, it make update faster and avoids UI bugs.
     }, 
 
     "Heading2"
@@ -26,20 +53,21 @@ console.log(heading);
 
 //want to put in heading 1 and heading 2
 const container = React.createElement(
-    "div", //name of the tag
-     { // this object take props like color , style, className
+    "div",                               //-------------name of the tag
+     {                                  // -----------this object take props like color , style, className
 
-    id: "container",
+    id: "container", 
+                     //----------core-attribute name(props)
 
-}, [heading, heading2 ] //This h1 ,h2 is React Elements (Object )
-); //To put multiple headings/Values (called childrens in React)use Array
+}, [heading, heading2 ]             //-----------This h1 ,h2 is React Elements (Object )
+);                                 //--------------To put multiple headings/Values (called childrens in React)use Array
 
 console.log(heading);
 
- //To Put above "h1" tag inside root---------------
+ //-------------------To Put above "h1" tag inside root---------------
  const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//passing a react element inside the root
+//-------------------passing a react element inside the root--------------------------
 //root.render(heading); //print heading hello world
 
 //This will Overwrite(Div root)
